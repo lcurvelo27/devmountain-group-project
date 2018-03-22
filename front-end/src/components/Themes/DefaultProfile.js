a19f56f7aa1cf59b11b13b35a20cb562eed3b4e8
Lcurvelo27 <lcurvelo27@gmail.com>
2018-03-21 19:04:48 -0600

    added resume structure. No styling

--- front-end/src/components/Themes/DefaultProfile.js
+++ front-end/src/components/Themes/DefaultProfile.js
@@ -6,31 +6,39 @@ const DefaultProfile = (props) => {
   return (
     <div>
       <Navbar/>
-      <img src={props.user.imgurl} alt={`${props.user.username} profile picture`}/>
-      <h1>{props.user.firstname} {props.user.lastname}</h1>
-      <h5>{props.user.description}</h5>
-      <hr/>
-      <h2>Skills</h2>
-        {props.user.skills.map(skill => {
-          return(
-            <div>
-              <h5>{skill.skill}: {skill.lvl}</h5>
-            </div>
-          )
-        })}
+      <div className='cv-container column-center'>
+        <img src={props.user.imgurl} alt={`${props.user.username} profile picture`} style={{'border-radius':100}}/>
+        <h1>{props.user.firstname} {props.user.lastname}</h1>
+        <h5>{props.user.description}</h5>
         <hr/>
-      <h2>Education</h2>
-      {props.user.education.map(school => {
-        return(
-          <div>
-            <h4>{school.school}</h4>
-            <h5>{school.emphasis}</h5>
-            <h6>Start Date: {school.start_date}</h6>
-            <h6>End Date: {school.end_date}</h6>
+        <div className='cv-content center'>
+          <div style={{'padding-left':150, 'border-left':'1px solid #8E8200', 'margin-left':150}}>
+            <h2>Skills</h2>
+              {props.user.skills.map(skill => {
+                return(
+                  <div>
+                    <h5>{skill.skill}: {skill.lvl}</h5>
+                  </div>
+                )
+              })}
+              <hr/>
+            <h2>Education</h2>
+            {props.user.education.map(school => {
+              return(
+                <div>
+                  <h4>{school.school}</h4>
+                  <h5>{school.emphasis}</h5>
+                  <h6>Start Date: {school.start_date}</h6>
+                  <h6>End Date: {school.end_date}</h6>
+                </div>
+              )
+            })}
           </div>
-        )
-      })}
-
+          <div>
+            <h2>Experience</h2>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
