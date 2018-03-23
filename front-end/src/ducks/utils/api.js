d55aba8abb38cbb4eef2801ccdd617070f6a58fc
Lcurvelo27 <lcurvelo27@gmail.com>
2018-03-22 18:42:01 -0600

    refactored profile component/ added redux

--- front-end/src/ducks/utils/api.js
+++ front-end/src/ducks/utils/api.js
@@ -3,5 +3,17 @@ var axios = require('axios')
 module.exports = {
 	getUsers: () => {
 		return axios.get('http://localhost:3005/api/search/tiles').then(response => response.data)
-	}
+	},
+	 getProfile(username) {
+    	return axios.get(`http://localhost:3005/api/users/${username}`).then(response => {
+    		if(response.data[0]){
+    			return response.data[0]
+    		}
+    		else {
+    			return null
+    		}
+    	}).catch(err => {
+        console.log(err)
+      })
+  	}
 }
