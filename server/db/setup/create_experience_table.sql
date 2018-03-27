b4e8fef48f4c233c0635d510c77a2a89d8bd76a1
Foster Irwin <jfosterirwin@gmail.com>
2018-03-27 08:37:21 -0600

    create experience table

---
+++ server/db/setup/create_experience_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE experience_table (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100),
+    company VARCHAR(100),
+    description VARCHAR(300),
+    start_date DATE,
+    end_date DATE,
+    authid VARCHAR(50) REFERENCES users(authid)
+)
