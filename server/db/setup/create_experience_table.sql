2ade36abfe3b82f64a0dca6955ba5e7c14249ff2
Foster Irwin <jfosterirwin@gmail.com>
2018-03-27 08:47:29 -0600

    updated table to add location column

--- server/db/setup/create_experience_table.sql
+++ server/db/setup/create_experience_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE experience_table (
     title VARCHAR(100),
     company VARCHAR(100),
     description VARCHAR(300),
+    location VARCHAR(100),
     start_date DATE,
     end_date DATE,
     authid VARCHAR(50) REFERENCES users(authid)
