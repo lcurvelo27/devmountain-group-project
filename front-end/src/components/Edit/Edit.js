1ac53e1567ddd7e04019510e69857736e5e145bb
Foster Irwin <jfosterirwin@gmail.com>
2018-03-31 14:18:18 -0600

    basic edit page, need to add theme and hook up to api calls using redux

---
+++ front-end/src/components/Edit/Edit.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import { connect } from 'react-redux'
+import Navbar from '../Navbar'
+import {getProfileDetails} from '../../ducks/action'
+import TextField from './TextField'
+import EducationFields from './EducationFields'
+
+class Profile extends Component{
+  componentDidMount() {
+    this.props.getProfileDetails(this.props.match.params.username)
+  }
+	render(){
+		return(
+			<div>
+        <Navbar user = {this.props.user}/>
+        {this.props.user
+          ?
+          <div>
+            Account
+            <TextField title="Image URL" value={this.props.user.imgurl}/>
+            <TextField title="Username" value={this.props.user.username}/>
+            <hr/>
+            Name
+            <TextField title="First" value={this.props.user.firstname}/>
+            <TextField title="Last" value={this.props.user.lastname}/>
+            <hr/>
+            Description
+            <TextField title="Bio" value={this.props.user.description}/>
+            <hr/>
+            Education
+            {this.props.user.education.map(school => {
+              return(
+                <div>
+                  <EducationFields value={school}/>
+                </div>
+              )
+            })}
+            {this.props.user.skills.map(skill => {
+              return(
+                <div>
+                  <TextField title="Skill" value={skill.skill}/>
+                  <TextField title="Level" value={skill.lvl}/>
+                </div>
+              )
+            })}
+          </div>
+          :
+          'Loading...'
+        }
+			</div>
+		)
+	}
+}
+
+const mapStateToProps = state => {
+	return {
+		user: state.user,
+    loading: state.loading
+	}
+}
+export default connect(mapStateToProps, {getProfileDetails})(Profile)
---
+++ front-end/src/components/Edit/SkillsFields.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+
+class TextField extends Component{
+  constructor() {
+    super()
+    this.state = {
+      skills: {
+        skill: null,
+        lvl: null,
+        id: null
+      }
+    }
+  }
+  componentDidMount() {
+    let skills = this.props.value
+    this.setState({skills})
+  }
+  updateValue(newValue) {
+    this.setState({skills: newValue})
+  }
+  cancelEdit() {
+    this.setState({skills: this.props.value})
+  }
+  render(){
+    return (
+      <div>
+        School Number
+        <div>
+          <label>
+            <div>
+              School<input type="text" value={this.state.skills.skill} onChange={(e) => this.updateValue(e.target.value)}/>
+            </div>
+            <div>
+              Emphasis<input type="text" value={this.state.skills.lvl} onChange={(e) => this.updateValue(e.target.value)}/>
+            </div>
+          </label>
+        </div>
+        {this.state.education !== this.props.value
+          ?
+          <div>
+          <button onClick={()=>this.cancelEdit()}>Cancel</button>
+          <button>Save</button>
+          </div>
+          :
+          null
+        }
+      </div>
+    )
+  }
+}
+export default TextField
