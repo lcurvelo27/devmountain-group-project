ade0e5138bcb3510ece2e68adbf500fb2f0f3d3f
Foster Irwin <jfosterirwin@gmail.com>
2018-03-31 14:17:44 -0600

    theme data points

--- server/routes/users.js
+++ server/routes/users.js
@@ -21,5 +21,22 @@ module.exports = function(app) {
       })
   })
 
+  router.put(`/:username`, (res, req) => {
+    app.get('db').users.update_user({
+      username: req.params.username,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      description: req.body.description,
+      imgurl: req.body.imgurl
+    })
+      .then(response => {
+        return res.status(200).send(response)
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).send('there was an error')
+      })
+  })
+
   return router
 }
