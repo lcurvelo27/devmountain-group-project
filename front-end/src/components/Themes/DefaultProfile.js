7c8a08ec1c8058f1b8119350beabca4e89ca995e
Lcurvelo27 <lcurvelo27@gmail.com>
2018-04-13 21:02:57 -0600

    deconstructed props.theme in defaultProfile component

--- front-end/src/components/Themes/DefaultProfile.js
+++ front-end/src/components/Themes/DefaultProfile.js
@@ -5,47 +5,48 @@ import {setTheme} from '../../ducks/action'
 
 
 const DefaultProfile = (props) => {
+  const {wrapper, cvContainer, imageDiv, titleColor, textColor, cvContent, rightPiece, leftPiece} = props.theme
   return (
-    <div style={props.theme.wrapper}>
-      <div style={props.theme.cvContainer}>
-        <div style={props.theme.imageDiv}>
+    <div style={wrapper}>
+      <div style={cvContainer}>
+        <div style={imageDiv}>
           <img src={props.user.imgurl} alt={`${props.user.username} profile picture`} style={{'borderRadius':100}}/>
-          <h1 style={props.theme.titleColor}>{props.user.firstname} {props.user.lastname}</h1>
-          <h5 style={props.theme.textColor}>{props.user.description}</h5>
+          <h1 style={titleColor}>{props.user.firstname} {props.user.lastname}</h1>
+          <h5 style={textColor}>{props.user.description}</h5>
         </div>
-        <div style={props.theme.cvContent}>
-          <div style={props.theme.rightPiece}>
-            <h2 style={props.theme.titleColor}>Skills</h2>
+        <div style={cvContent}>
+          <div style={rightPiece}>
+            <h2 style={titleColor}>Skills</h2>
               {props.user.skills.map(skill => {
                 return(
                   <div key={skill.skill}>
-                    <h5 style={props.theme.textColor}>{skill.skill}: {skill.lvl}</h5>
+                    <h5 style={textColor}>{skill.skill}: {skill.lvl}</h5>
                   </div>
                 )
               })}
-            <h2 style={props.theme.titleColor}>Education</h2>
+            <h2 style={titleColor}>Education</h2>
             {props.user.education.map(school => {
               return(
                 <div key={school.school}>
-                  <h4 style={props.theme.titleColor}>{school.school}</h4>
-                  <h5 style={props.theme.titleColor}>{school.emphasis}</h5>
-                  <h6 style={props.theme.textColor}>Start Date: {school.start_date}</h6>
-                  <h6 style={props.theme.textColor}>End Date: {school.end_date}</h6>
+                  <h4 style={titleColor}>{school.school}</h4>
+                  <h5 style={titleColor}>{school.emphasis}</h5>
+                  <h6 style={textColor}>Start Date: {school.start_date}</h6>
+                  <h6 style={textColor}>End Date: {school.end_date}</h6>
                 </div>
               )
             })}
           </div>
-          <div style={props.theme.leftPiece}>
-            <h2 style={props.theme.titleColor}>Experience</h2>
+          <div style={leftPiece}>
+            <h2 style={titleColor}>Experience</h2>
             {props.user.experience.map(experience => {
               return(
                 <div key={experience.id}>
-                  <h4 style={props.theme.titleColor}>{experience.title}</h4>
-                  <h5 style={props.theme.textColor}>{experience.company}</h5>
-                  <h5 style={props.theme.textColor}>{experience.location}</h5>
-                  <h5 style={props.theme.textColor}>{experience.description}</h5>
-                  <h6 style={props.theme.textColor}>Start Date: {experience.start_date}</h6>
-                  <h6 style={props.theme.textColor}>End Date: {experience.end_date}</h6>
+                  <h4 style={titleColor}>{experience.title}</h4>
+                  <h5 style={textColor}>{experience.company}</h5>
+                  <h5 style={textColor}>{experience.location}</h5>
+                  <h5 style={textColor}>{experience.description}</h5>
+                  <h6 style={textColor}>Start Date: {experience.start_date}</h6>
+                  <h6 style={textColor}>End Date: {experience.end_date}</h6>
                 </div>
               )
             })}
