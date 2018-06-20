542454bd9ddf77533bbf5a487b7b623d2185249e
Lcurvelo27 <lcurvelo27@gmail.com>
2018-06-19 19:06:28 -0600

    fixed router

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -3,20 +3,17 @@ import {Switch, Route, Redirect} from 'react-router-dom'
 import Search from './components/Search'
 import Profile from './components/Profile'
 import Home from './components/Home'
-import Edit, { checkAuth } from './components/Edit/Edit'
+import Edit from './components/Edit/Edit'
 import PrivateRoute from './components/PrivateRouter'
 import Login from './components/Login'
-import axios from 'axios'
-
-
 
 
 export default(
 	<Switch>
 		<Route path='/' exact component={Home} />
 		<Route path='/search' component={Search} />
-		<Route path='/profile/:username' component={Profile} />
+		<PrivateRoute path='/edit' component={Edit} exact/>
 		<Route path='/login' component={Login} exact/>
-		<PrivateRoute path='/edit' component={Edit}/>
+		<Route path='/:username' component={Profile} />
 	</Switch>
 )
