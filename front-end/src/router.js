13ad4a28b8dcf2344b7b69b30fe2cdce92c424c8
lcurvelo27 <32043573+lcurvelo27@users.noreply.github.com>
2018-06-20 20:01:54 -0600

    Update router.js

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Switch, Route, Redirect} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 import Search from './components/Search'
 import Profile from './components/Profile'
 import Home from './components/Home'
