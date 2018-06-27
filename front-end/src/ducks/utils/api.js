11ee50efaf4d86a00be91a42e6187c57d50b6ccc
Foster Irwin <jfosterirwin@gmail.com>
2018-06-27 09:29:20 -0600

    updated axios calls to pass with credentials for editing

--- front-end/src/ducks/utils/api.js
+++ front-end/src/ducks/utils/api.js
@@ -15,34 +15,34 @@ module.exports = {
     return axios.get(`http://localhost:3005/api/users/${username}`)
   },
 	addEducation: (details) => {
-		return axios.post(`http://localhost:3005/api/users/education`, details)
+		return axios.post(`http://localhost:3005/api/users/education`, details, {withCredentials: true})
 	},
 	updateEducation: (details) => {
-		return axios.put(`http://localhost:3005/api/users/education`, details)
+		return axios.put(`http://localhost:3005/api/users/education`, details, {withCredentials: true})
 	},
 	deleteEducation: (details) => {
-		return axios.delete(`http://localhost:3005/api/users/education`, {data: details})
+		return axios.delete(`http://localhost:3005/api/users/education`, {data: details, withCredentials: true})
 	},
 	addSkill: (details) => {
-		return axios.post(`http://localhost:3005/api/users/skills`, details)
+		return axios.post(`http://localhost:3005/api/users/skills`, details, {withCredentials: true})
 	},
 	updateSkill: (details) => {
-		return axios.put(`http://localhost:3005/api/users/skills`, details)
+		return axios.put(`http://localhost:3005/api/users/skills`, details, {withCredentials: true})
 	},
 	deleteSkill: (details) => {
-		return axios.delete(`http://localhost:3005/api/users/skills`, {data: details})
+		return axios.delete(`http://localhost:3005/api/users/skills`, {data: details, withCredentials: true})
 	},
 	addExperience: (details) => {
-		return axios.post(`http://localhost:3005/api/users/experience`, details)
+		return axios.post(`http://localhost:3005/api/users/experience`, details, {withCredentials: true})
 	},
 	updateExperience: (details) => {
-		return axios.put(`http://localhost:3005/api/users/experience`, details)
+		return axios.put(`http://localhost:3005/api/users/experience`, details, {withCredentials: true})
 	},
 	deleteExperience: (details) => {
-		return axios.delete(`http://localhost:3005/api/users/experience`, {data: details})
+		return axios.delete(`http://localhost:3005/api/users/experience`, {data: details, withCredentials: true})
 	},
 	updateUser: (user) => {
 		console.log(user)
-		return axios.put(`http://localhost:3005/api/users`, user)
+		return axios.put(`http://localhost:3005/api/users`, user, {withCredentials: true})
 	}
 }
