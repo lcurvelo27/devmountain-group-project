e691c2640fa7688f7621eb86f29129ed0dc124c1
Foster Irwin <jfosterirwin@gmail.com>
2018-06-27 20:43:22 -0600

    auth edits

--- server/routes/Authenthication.js
+++ server/routes/Authenthication.js
@@ -1,48 +1,46 @@
 module.exports = function(app) {
-    require('dotenv').config()
-    const express = require('express')
-    const router = express.Router()
-    const passport = require('passport')
-    const Auth0Strategy = require('passport-auth0')
-    const cookieParser = require('cookie-parser')
-    const session = require('express-session')
+  require('dotenv').config()
+  const express = require('express')
+  const router = express.Router()
+  const passport = require('passport')
+  const Auth0Strategy = require('passport-auth0')
+  const cookieParser = require('cookie-parser')
+  const session = require('express-session')
 
-    app.use(session({
-      secret: process.env.SECRET,
-      resave: false,
-      saveUninitialized: false
-    }));
-  
-    const strategy = new Auth0Strategy(
-      {
-        domain: process.env.AUTH0_DOMAIN,
-        clientID: process.env.AUTH0_CLIENT_ID,
-        clientSecret: process.env.SECRET,
-        callbackURL: process.env.AUTH0_CALLBACK_URL
-      },
-      (accessToken, refreshToken, extraParams, profile, done) => {
-        app.get('db').users.get_user_session({authid: profile.id})
-          .then(response => {
-            console.log(response)
-            if(response.length == 0){
-              console.log('its created')
-              app.get('db').users.create_user({authid: profile.id})
-                .then(response => {
-                  done (null, profile.id)
-                })
-            }
-            else{
-              done(null, response[0].authid)
-            }
-          })
-      }
-    );
-    
-  
-  passport.use(strategy);
+  app.use(cookieParser(process.env.SESSION_SECRET))
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  }));
+
+  passport.use(new Auth0Strategy({
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL: process.env.AUTH0_CALLBACK_URL,
+    scope: 'openid'
+    },
+    (accessToken, refreshToken, extraParams, profile, done) => {
+      app.get('db').users.get_user_session({authid: profile.id})
+        .then(user => {
+          console.log(27, user)
+          if(user[0]) {
+            done(null, {authid: profile.id})
+          } else {
+            app.get('db').users.create_user({authid: profile.id})
+              .then(user => {
+                console.log(33, user)
+                done(null, {authid: profile.id})
+              })
+          }
+        })
+  }))
+
+  app.use(passport.initialize())
+  app.use(passport.session())
 
   passport.serializeUser(function(user, done){
-    console.log('this is the session', user)
     done(null, user);
   })
 
@@ -50,63 +48,48 @@ module.exports = function(app) {
     done(null, user);
   })
 
-  app.use(passport.initialize())
-  app.use(passport.session())
-
-      router.get(
-        '/login',
-      passport.authenticate('auth0', {
-        clientID: process.env.AUTH0_CLIENT_ID,
-        domain: process.env.AUTH0_DOMAIN,
-        redirectUri: 'http://localhost:3005/auth/callback',
-        audience: 'https://' + process.env.AUTH0_DOMAIN + '/userinfo',
-        responseType: 'code',
-        scope: 'openid'
-      }),
-    
-      function(req, res) {  
-        res.redirect('/');
-        }
-      )
-  
-        router.get(
-          '/callback',
-        passport.authenticate('auth0', {
-            failureRedirect: '/'
-        }),
-        function(req, res) {
-          console.log(req.user)
-          if(req.user){
-            res.redirect(req.session.returnTo || 'http://localhost:3000/#/');
+  router.get('/login',
+    passport.authenticate('auth0', {
+      clientID: process.env.AUTH0_CLIENT_ID,
+      domain: process.env.AUTH0_DOMAIN,
+      redirectUri: process.env.AUTH0_CALLBACK_URL,
+      scope: 'openid'
+    }),
+    (req, res) => {
+      res.redirect('/edit')
+    }
+)
+  router.get('/callback', passport.authenticate('auth0', {
+    failureRedirect: process.env.AUTH0_FAILURE_URL
+  }),
+  function(req, res) {
+    if(!req.user) {
+      throw new Error('user null')
+    }
+    res.redirect(process.env.AUTH0_SUCCESS_URL)
+  }
+)
+  router.get(
+    '/me',
+    function(req, res){
+      app.get('db').users.get_user_session(req.user).then(
+        response => {
+          if(response.length > 0){
+            res.status(200).send(response)
           }
           else{
-            res.redirect('http://localhost:3000/#/edit/testuser')
+            res.status(401).send('unauthorized')
           }
-        }
-      );
+    })})
 
-      router.get(
-        '/me',
-        function(req, res){
-          app.get('db').users.get_user_session(req.user).then(
-            response => {
-              if(response.length > 0){
-                res.status(200).send(response)
-              }
-              else{
-                res.status(401).send('unauthorized')
-              }
-        })})
+  router.get('/userEdit', (req, res) => {
+    console.log('this is userEDit', req.session.passport.user.authid)
+    app.get('db').users.get_user_edit({authid: req.session.passport.user.authid})
+      .then(response => {
+        console.log(response)
+        return res.status(200).send(response)
+      })
+  })
 
-        router.get('/userEdit', (req, res) => {
-          console.log('this is userEDit', req.user)
-          app.get('db').users.get_user_edit(req.user)
-            .then(response => {
-              console.log(response)
-              return res.status(200).send('test')
-            })
-        })
-
-    return router
+  return router
   }
-  
